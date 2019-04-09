81ab40015b9a6640a722b6aa98d5d4c566ddd408
youssef <youssef.esmaileffekygmail.com@youssefs-mac.local>
2019-04-09 22:53:52 +0200

    Initial Commit

---
+++ toDayCoreData/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  toDayCoreData
+//
+//  Created by youssef on 4/9/19.
+//  Copyright © 2019 youssef. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
